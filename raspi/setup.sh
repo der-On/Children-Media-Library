38aa239ea6603be044f94f71684dba324f461715
Ondrej Brinkel <info@anzui.de>
2017-12-13 20:46:26 +0100

    updated setup script

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -3,8 +3,16 @@
 cd "$(dirname "$0")"
 cd ..
 
+echo "Installing nodejs ..."
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install nodejs
+
+echo "Installing latest npm and pm2 ..."
 sudo npm install npm pm2 -g
+
+echo "Installing package dependencies ..."
 npm install
+
+echo "Adding startup script to /etc/rc.local"
+echo "$(dirname "$0")/start.sh" >> /etc/rc.local
