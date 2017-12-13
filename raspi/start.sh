0a0377022df51d8fa891e80c7918ae8a982aed5b
Ondrej Brinkel <info@anzui.de>
2017-12-13 20:55:00 +0100

    updated start script to work with chromium on raspi

--- raspi/start.sh
+++ raspi/start.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 cd ..
 
 pm2 start --name "children-audio-library" server.js
-chromium --disable-pinch --overscoll-history-navigation=0 --kiosk http://localhost:8000
+/usr/bin/chromium-browser --disable-pinch --overscoll-history-navigation=0 --kiosk http://localhost:8000
 pm2 delete "children-audio-library"
