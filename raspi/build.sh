dcfe0dab6585a2938cb534aa8347c4a0f696ea46
Ondrej Brinkel <info@anzui.dev>
2021-12-30 21:20:49 +0100

    fixes raspi build

--- raspi/build.sh
+++ raspi/build.sh
@@ -4,9 +4,9 @@ cd "$(dirname "$0")"
 cd ..
 
 docker run \
-    --volume ${PWD}:/home/cross/project \
-    ragnaroek/rust-raspberry:1.42.0 \
-    build --release
+   --volume ${PWD}:/home/cross/project \
+   ragnaroek/rust-raspberry:1.57.0 \
+   build --release
 
-cp -R target/arm-unknown-linux-gnueabihf/release/cli ./raspi/bin/cli
+cp -R target/arm-unknown-linux-gnueabihf/release/cli ./raspi/bin/cli && \
 cp -R target/arm-unknown-linux-gnueabihf/release/server ./raspi/bin/server
